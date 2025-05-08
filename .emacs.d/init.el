e0162f5e0814b30e67da63b6b97c011c8821ad5a
Subie Patel <subiepatel@microsoft.com>
2025-05-07 18:26:20 -0700

    turn off copilot-mode
    Tab completion interferes with indentation.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -123,7 +123,7 @@
             :rev :newest
             :branch "main")
   :config
-  (add-hook 'prog-mode-hook 'copilot-mode)
+  ;;(add-hook 'prog-mode-hook 'copilot-mode)
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
 
